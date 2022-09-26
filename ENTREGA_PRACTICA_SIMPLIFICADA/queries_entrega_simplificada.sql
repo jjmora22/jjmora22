6aa068c25ffa57a493d3c5617fc7c71a46951c9d
jjmora22 <99255848+jjmora22@users.noreply.github.com>
2022-09-26 22:56:16 +0200

    Update queries_entrega_simplificada.sql
    Un solo query según solicitado en práctica

--- ENTREGA_PRACTICA_SIMPLIFICADA/queries_entrega_simplificada.sql
+++ ENTREGA_PRACTICA_SIMPLIFICADA/queries_entrega_simplificada.sql
@@ -1,43 +1,6 @@
--- nombre modelo, marca y grupo de coches del inventario de keepcoding 
+-- Query SQL según solicitado en práctica 
 
-select b.modelo, c.marca, d.grupo
-from nueva_entrega_simplificada.auto a, nueva_entrega_simplificada.modelo b, nueva_entrega_simplificada.marca c, nueva_entrega_simplificada.grupo d
-where a.codigo_modelo = b.codigo_modelo and b.codigo_marca = c.codigo_marca and c.id_grupo = d.id_grupo; 
-
-
--- fecha de compra (agrego columnas de modelo, marca y grupo, para leerlo mejor)
-
-select b.modelo, c.marca, a.matricula, a.fecha_compra 
-from nueva_entrega_simplificada.auto a, nueva_entrega_simplificada.modelo b, nueva_entrega_simplificada.marca c
-where a.codigo_modelo = b.codigo_modelo and b.codigo_marca = c.codigo_marca; 
-
--- matrícula (igualmente, con las columnas modelo, marca y grupo, para leerlo mejor)
-
-select b.modelo, c.marca, a.matricula 
-from nueva_entrega_simplificada.auto a, nueva_entrega_simplificada.modelo b, nueva_entrega_simplificada.marca c
-where a.codigo_modelo = b.codigo_modelo and b.codigo_marca = c.codigo_marca;
-
-
--- Nombre del color del coche
-select b.modelo, c.marca, a.matricula,  d.color 
-from nueva_entrega_simplificada.auto a, nueva_entrega_simplificada.modelo b, nueva_entrega_simplificada.marca c, nueva_entrega_simplificada.colores d
-where a.codigo_modelo = b.codigo_modelo and b.codigo_marca = c.codigo_marca and a.codigo_color = d.codigo_color;
-
--- total kilómetros --> Primero la relación y luego el total 
-select b.modelo, c.marca, a.matricula,  a.kilometros
-from nueva_entrega_simplificada.auto a, nueva_entrega_simplificada.modelo b, nueva_entrega_simplificada.marca c
-where a.codigo_modelo = b.codigo_modelo and b.codigo_marca = c.codigo_marca;
-
--- ahora el total de la sumatoria de los kilómetros de todos los coches
-select sum(kilometros) as "suma de kilómetros" from nueva_entrega_simplificada.auto;
-
--- ahora el nombre de las empresas en la que está asegurado cada coche
-select b.modelo, c.marca, a.matricula,  d.compannia_seguros 
-from nueva_entrega_simplificada.auto a, nueva_entrega_simplificada.modelo b, nueva_entrega_simplificada.marca c, nueva_entrega_simplificada.seguro d
-where a.codigo_modelo = b.codigo_modelo and b.codigo_marca = c.codigo_marca and a.numero_poliza = d.numero_poliza;
-
--- y ahora agrego el numero de poliza (dejo el nombre de la compañía para facilitar lectura --quito marca para dar más visibilidad)
-select b.modelo, a.matricula,  d.numero_poliza 
-from nueva_entrega_simplificada.auto a, nueva_entrega_simplificada.modelo b, nueva_entrega_simplificada.seguro d
-where a.codigo_modelo = b.codigo_modelo and a.numero_poliza = d.numero_poliza;
+select b.modelo, c.marca, d.grupo, a.fecha_compra, a.matricula, e.color, a.kilometros, f.compannia_seguros, f.numero_poliza
+from nueva_entrega_simplificada.auto a, nueva_entrega_simplificada.modelo b, nueva_entrega_simplificada.marca c, nueva_entrega_simplificada.grupo d, nueva_entrega_simplificada.colores e, nueva_entrega_simplificada.seguro f
+where a.codigo_modelo = b.codigo_modelo and b.codigo_marca = c.codigo_marca and c.id_grupo = d.id_grupo and a.codigo_color = e.codigo_color and a.numero_poliza = f.numero_poliza; 
 
